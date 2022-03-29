53b956a9e50bd038e6acb5a32a4ea715346e88b4
Marianolavarra21817 <62063011+Marianolavarra21817@users.noreply.github.com>
2022-03-29 17:13:31 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Swict, route  } from 'react-router-dom';
+import NavBar from "./Components/Navegacion/NavBar";
+import Inicio from './Components/Paginas/Inicio';
+import Coach from './Components/Paginas/Coach';
+import Items from './Components/Paginas/Items';
 
-function App() {
+ function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+    <>
+    <Router>
+      <NavBar/>
+      <switch>
+        <route path='/' exact component={Inicio}/>
+        <route path='/Coach' exact component={Coach}/>
+        <route path='/Items' exact component={Items}/>
+      </switch>
+    </Router>
+    </>
     </div>
-  );
-}
+    )
+  };
+
 
 export default App;
---
+++ src/Components/Navegacion/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import CartWidget from '../CartWidget/CartWidget'
+const NavBar = () => {
+  return (
+  <>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   
+        <img  src="./INNOVAC.jpg" width='200' ></img>
+    
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mx-auto">
+      <Link class="nav-link" to ='/'> Inicio </Link>
+      <Link class="nav-link" to='/Coach'> Coach</Link>
+      <Link class="nav-link" to='/Items' > Items</Link>
+  
+    </div>
+      <CartWidget/>
+  </div>
+</nav>
+</>   
+  )
+  
+}
+
+export default NavBar
